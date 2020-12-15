49899a16c6a48797da5f042a9c06cc3db21e2262
Wel <1195112836@qq.com>
2020-12-16 02:12:42 +0800

    加入必要模块

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/css/reset.css'
+import './assets/css/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body) //解决移动端部分机型点击事件延迟300ms问题
 
 /* eslint-disable no-new */
 new Vue({
