bd4ca5a7603f8674ec1847f02bc6a82c221a480c
Wel <1195112836@qq.com>
2020-12-16 23:00:31 +0800

    代码

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont back">&#xe66c;</span>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe684;</span>
+            输入城市/景点/游玩主题 
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont">&#xe686;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~assets/styles/common.styl'
+    .header
+        line-height .86rem
+        width 7.5rem
+        display flex
+        background-color $bgColor
+        color #fff
+        .header-left 
+            width .64rem
+            float left 
+            text-align center
+            span 
+                font-size .4rem
+        .header-input
+            flex 1
+            background #ffffff
+            border-radius 5px
+            height .64rem
+            line-height .64rem
+            margin-top .12rem
+            margin-left .10rem
+            color #ccc
+            padding-left .2rem
+        .header-right
+            width 1.24rem
+            float right
+            text-align center
+</style>
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="wrapper">
+        <swiper ref="mySwiper" :options="swiperOptions">
+            <swiper-slide  v-for="item in swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="圆明园" />
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "HomeSwiper",
+    data(){
+        return{
+            swiperOptions:{
+                pagination:".swiper-pagination",
+                loop:true
+            },
+            swiperList:[{
+                id:"0001",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg"
+            },{
+                id:"0002",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/8216fd250c35366befc35f6353f934b9.jpg_890x330_9062b3ba.jpg"
+            },{
+                id:"0003",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg"
+            },{
+                id:"0004",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/8216fd250c35366befc35f6353f934b9.jpg_890x330_9062b3ba.jpg"
+            },{
+                id:"0005",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg"
+            },{
+                id:"0006",
+                imgUrl:"https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/8216fd250c35366befc35f6353f934b9.jpg_890x330_9062b3ba.jpg"
+            },
+            
+            
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff !important
+    .wrapper
+        width 100%
+        height 0
+        overflow hidden
+        padding-bottom 37%
+        background-color #ccc
+        
+        .swiper-img
+            width 100%
+</style>
