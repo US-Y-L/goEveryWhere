f33569a980abd2a0ec07feb4ab4c942a31db4583
Wel <1195112836@qq.com>
2020-12-16 21:52:58 +0800

    添加头部模块

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 import fastClick from 'fastclick'
 import './assets/css/reset.css'
 import './assets/css/border.css'
+import "./utils/rem"
+import "./assets/css/iconfont.css"
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) //解决移动端部分机型点击事件延迟300ms问题
